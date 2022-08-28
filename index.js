de8087815dc6df39f2196b6ad5fe752470e29945
Machoka Joash Omao <aaronjoash44@gmail.com>
2022-08-28 06:20:07 +0300

    completed phase-1 sending data with fetch

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
-// Add your code here
+const userDiv = document.getElementById('userDiv');
+console.log(userDiv)
+
+function submitData(name, email) {
+    return fetch('http://localhost:3000/users', {
+        method: "POST",
+        headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+        },
+        body: JSON.stringify({
+            name: name,
+            email: email,
+        }),
+    })
+    .then(response => response.json())
+    .then(user => {
+        userDiv.innerHTML = `
+        <h2>User ID</h2>
+        ${user.id}
+        `
+        
+    })
+    .catch((error) => {
+        userDiv.append(error.message);
+    })
+}
+
+submitData('steve', 'steve@steve.com')
+
+
+// {o_<} //
